45cd0532b654c697c8a274dda7d166be18cce1e7
Innova repo <pavandesineedi877@gmail.com>
2024-07-04 12:25:14 +0530

    "Example 1st repo"

---
+++ class.py
@@ -0,0 +1,14 @@
+'''
+class innova():
+    a=30
+    def output(self):
+        print(self.a)
+b=innova()
+b.output()
+'''
+class innova():
+    a=30
+    def output(self):
+         print(self.a)
+b=innova()
+b.output()
---
+++ lambdanw.py
@@ -0,0 +1,20 @@
+#sum = lambda a,b:a+b
+#print(sum(9,8))
+
+#square =lambda a:a*a
+#print(square(6))
+
+#list=[2,4,7,8,10,11]
+#new_list= list(filter(lambda x:(x%2 ==0),list))
+#print(list)
+my_list = [1, 5, 4, 6, 8, 11, 3, 12]
+
+new_list = list(filter(lambda x: (x%2 == 0) , my_list))
+
+print(new_list)
+
+
+
+
+
+
